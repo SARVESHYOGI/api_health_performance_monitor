4577560ccee9b58b449617af6d9bc19ef297a33f
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2026-01-13 00:10:30 +0530

    node init

---
+++ backend/nodejs/app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const pool = require('./db');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
