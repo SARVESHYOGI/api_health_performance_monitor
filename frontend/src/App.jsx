07a76bc196290b13efeaa7854b66225b6374387b
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2026-01-15 22:22:05 +0530

    added extension

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
-import Dashboard from './pages/dashboard'
-import Login from './pages/Login'
-import Register from './pages/Register'
+import Dashboard from './pages/dashboard.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
